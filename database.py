add892aa9257768a6553cb20723163db2d782537
Lucas <lucas.ribeiro@smarttbotter.com>
2021-12-06 20:30:26 -0300

    Procfile Heroku

--- database.py
+++ database.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./sql_app.db"
+SQLALCHEMY_DATABASE_URL = "sqlite:///./sqlapp.db"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
@@ -12,4 +12,4 @@ SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
 
-__all__= ["SessionLocal", "engine", "Base"]
+__all__= ["SessionLocal", "engine", "Base"] 
